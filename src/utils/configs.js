a246cbce62910d2e136c93b23550fc0820763dba
Nicolas Lma <teixlima@gmail.com>
2020-06-22 21:43:04 -0300

    removing some logic from controller

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -4,14 +4,13 @@ const serverConfig = {
   dev: {
     REDIS: {
       TTL: 60000, //one minute
-      URL: process.env.REDIS_URL || false,
-    //URL: process.env.REDIS_URL || 'redis://localhost:6379'  Use this config if you are able to use redis locally
-    },
+      //URL: process.env.REDIS_URL || false,
+      URL: process.env.REDIS_URL || 'redis://localhost:6379'  },
     API_RESPONSE_LIMIT: 500,
     GIT_V4: 'https://api.github.com/graphql',
     GIT_PUB_TOKEN: 'Bearer 762f41e51efb51017e0f9228d31c2e6303c618ec',
     ROUTE: '/repositories/',
-    PORT: PORT || '3000',
+    PORT: '4000',
     ERRORS: {
       NOT_FOUND: 'RESOURCE_NOT_FOUND',
     },
